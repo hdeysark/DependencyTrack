3c81a08e35c1f57021e5addee080c4667e02ea7e
Hiya Dey Sarkar <hsarkar@cloudera.com>
2024-07-22 15:45:34 +0530

    added cleaup

---
+++ cleanup.py
@@ -0,0 +1,60 @@
+import requests
+import datetime
+import os 
+
+def get_api_key():
+    return os.getenv('API_KEY')
+
+def get_site_url():
+    return os.getenv('BASE_URL')
+
+deptrack_api_key = get_api_key()
+deptrack_baseurl = get_site_url()
+
+def cleanup():
+    url = deptrack_baseurl + "/v1/project?page=1&size=1000"
+    headers = {
+        'X-API-Key': deptrack_api_key
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        print(response)
+        if(response.status_code != 200):
+            return "Cannot fetch project list for cleanup", 400
+        
+        data = response.json()
+
+        for entry in data:
+            if entry.get('lastBomImport'):
+                last_bom_import_timestamp = entry.get('lastBomImport')
+                last_bom_import_seconds = last_bom_import_timestamp / 1000
+                last_bom_import_date = datetime.datetime.fromtimestamp(last_bom_import_seconds)
+                current_date = datetime.datetime.now()
+                time_difference = current_date - last_bom_import_date
+                one_week = datetime.timedelta(minutes=1)
+
+                if time_difference >= one_week:
+                    print("Last BOM import is older than a week.")
+                    uuid = entry.get('uuid')
+                    print(uuid)
+                    delete_url = deptrack_baseurl + "/v1/project/" + uuid
+                    delete_response = requests.delete(delete_url, headers=headers)
+                    print(delete_response)
+                else:
+                    print("Last BOM import is within a week.")
+
+            # Do we need this?
+            else:
+                print("No info on last BOM import, so delete")
+                uuid = entry.get('uuid')
+                print(uuid)
+                delete_url = deptrack_baseurl + "/v1/project/" + uuid
+                delete_response = requests.delete(delete_url, headers=headers)
+                print(delete_response)
+
+        return "Success", 200
+
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return "Unable cleanup", 400
