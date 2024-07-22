c7c389d6391ccfb380c417333992d6165596ed44
Hiya Dey Sarkar <hsarkar@cloudera.com>
2024-07-22 15:06:48 +0530

    Importing Sbom to dependency track

--- importstack.py
+++ importstack.py
@@ -21,8 +21,6 @@ def get_build_number():
 
 deptrack_api_key = get_api_key()
 deptrack_baseurl = get_site_url()
-stack_name = get_stack_name()
-build_number = get_build_number()
 
 stacks = {
     'CDH': 'parcel',
@@ -108,7 +106,7 @@ def DownloadSbom(stack_name, build_number):
 
         # os.makedirs(output_dir, exist_ok=True)
         
-        output_dir = "src/sboms/" + stack_name + "-" + stack_version
+        output_dir = "sboms/" + stack_name + "-" + stack_version
         subprocess.run(["wget", "-np", "-P", output_dir, "-r", "-R", "index.html*", url])
 
         print ('The SBOMs have been downloaded locally')
@@ -142,10 +140,119 @@ def find_sbom_files(directory):
                     print(f'Invalid JSON file skipped: {file_path}')
     return sbom_files
 
-getStacks()
-print(stacks)
+def merge_sbom_files(directory):
 
-print(f"Stack Name: {stack_name}")
-print(f"Build Number: {build_number}")
+    files = os.listdir(directory)
 
-DownloadSbom(stack_name, build_number)
+    sbom_file_paths = find_sbom_files(directory)
+
+    if not sbom_file_paths:
+        print('No valid SBOM files found in the directory.')
+
+    command = ['cyclonedx', 'merge', '--input-files'] + sbom_file_paths
+    output_file = os.path.join(directory, 'merged-sbom.json')
+    command += ['--output-file', output_file]
+
+    try:
+        print('merging')
+        result = subprocess.run(command, check=True, capture_output=True, text=True)
+        print(f'Successfully merged SBOM files into {output_file}')
+        print('Output:', result.stdout)
+        return output_file
+    except subprocess.CalledProcessError as e:
+        print('Failed to merge SBOM files.')
+        print('Error:', e.stderr)
+        print("error:")
+
+def importSBOM(stack_name, build_number):
+    download = DownloadSbom(stack_name, build_number)
+    if download != 200:
+        return "Unable to download SBOMs", 400
+    # download.get(stack_name=stack_name, build_number=build_number)
+
+    stack_version = build_number.split('-')[0]
+    
+    headers = {
+        'X-API-Key': deptrack_api_key
+    }
+
+    try:
+        url = deptrack_baseurl + "/v1/project/lookup?name=" + stack_name + "&version=" + stack_version
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            print("Project present")
+            uuid = response.json()['uuid']
+            delete_url = deptrack_baseurl + "/v1/project/" + uuid
+            delete_response = requests.delete(delete_url, headers=headers)
+            print("project deleted")
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+    dir = "sboms/" + stack_name + "-" + stack_version
+
+    output_file_merged = merge_sbom_files(dir)
+    print(output_file_merged)
+
+    url = deptrack_baseurl + "/v1/bom"
+    files = {
+        'autoCreate': True,
+        'projectName': stack_name,
+        'projectVersion': stack_version,
+        'bom': (open(output_file_merged, 'rb'))
+    } 
+
+    response = requests.post(url, headers=headers, files=files)
+    print("project created and bom uploaded")
+    try:
+        os.remove(output_file_merged)
+        print(f'Merged SBOM file deleted: {output_file_merged}')
+    except OSError as e:
+        print(f'Error deleting merged SBOM file: {e.strerror}')
+
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            project_file = file
+            if stack_name == "CDH":
+                if not file.endswith("binary.json"):
+                    continue
+            
+            projectName = project_file.strip('.json')
+            projectVersion = build_number
+            
+            print(f"file path - {os.path.join(root, project_file)}")
+
+            print(project_file, file)
+            files = {
+                'autoCreate': True,
+                'projectName': projectName,
+                'projectVersion': projectVersion,
+                'parentName': stack_name,
+                'parentVersion': stack_version,
+                'bom': (project_file, open(os.path.join(root, project_file), 'rb'))
+            }
+
+            response = requests.post(url, headers=headers, files=files)
+            print("bom uploaded")
+            print(response.text)
+
+    try:
+        shutil.rmtree(dir)
+        print(f"Directory and contents removed: {dir}")
+    except OSError as e:
+        print(f"Error removing directory: {dir} - {e.strerror}")
+
+    return "Stack uploaded on Dependency Track", 201
+    
+
+if __name__ == '__main__':
+
+    getStacks()
+    print(stacks)
+
+    stack_name = get_stack_name()
+    build_number = get_build_number()
+
+    print(f"Stack Name: {stack_name}")
+    print(f"Build Number: {build_number}")
+
+    importSBOM(stack_name, build_number)
